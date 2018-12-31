8349ae339130643f7b4697566e0e913edd94d0fa
Patrick Pflughaupt <43568412+patrickpflughaupt@users.noreply.github.com>
2018-12-31 09:28:28 +0000

    Update Tic_Tac_Toe.py

--- Tic_Tac_Toe.py
+++ Tic_Tac_Toe.py
@@ -8,7 +8,8 @@ import random
 
 #------------------------------------------------------------------------------
 
-# Define the board
+# Define the board that holds the data
+# If the board is full (i.e. no spaces left), then the game is finished
 board = ["", " ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 # Print the header
@@ -38,7 +39,26 @@ def print_board():
 	print("   |   |   ")		
 	print(" "+board[7]+" | "+board[8]+" | "+board[9]+"  ")
 	print("   |   |   ")
+    
+def is_player_winner(board, player):    
+    if(board[1] == player and board[2] == player and board[3] == player) or \
+        (board[4] == player and board[5] == player and board[6] == player) or \
+        (board[7] == player and board[8] == player and board[9] == player) or \
+        (board[1] == player and board[4] == player and board[7] == player) or \
+        (board[2] == player and board[5] == player and board[8] == player) or \
+        (board[3] == player and board[6] == player and board[9] == player) or \
+        (board[1] == player and board[5] == player and board[9] == player) or \
+        (board[3] == player and board[5] == player and board[7] == player):
+            return True
+    else:
+        return False
 	
+def is_board_full(board):
+    if " " in board:
+        return False
+    else:
+        return True
+
 while True:
     os.system("clear")
     print_header()
@@ -56,29 +76,15 @@ while True:
         time.sleep(1)
         
     # Check for X win
-    if(board[1] == "X" and board[2] == "X" and board[3] == "X") or \
-        (board[4] == "X" and board[5] == "X" and board[6] == "X") or \
-        (board[7] == "X" and board[8] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[4] == "X" and board[7] == "X") or \
-        (board[2] == "X" and board[5] == "X" and board[8] == "X") or \
-        (board[3] == "X" and board[6] == "X" and board[9] == "X") or \
-        (board[1] == "X" and board[5] == "X" and board[9] == "X") or \
-        (board[3] == "X" and board[5] == "X" and board[7] == "X"):
+    if is_player_winner(board, "X"):
             os.system("clear")
             print_header()
             print_board()
             print("X wins! Congratulations.")
             break
 
-    # Check for a tie (is the board full?)
-    is_board_full = True
-    for i in range(1, 10):
-        if board[i] == " ":
-            is_board_full = False
-            break
- 
     # If board is full, then do this
-    if is_board_full == True:
+    if is_board_full(board):
         print("Tie!")
         break
 
@@ -98,28 +104,14 @@ while True:
         time.sleep(1)
         
     # Check for X win
-    if(board[1] == "O" and board[2] == "O" and board[3] == "O") or \
-        (board[4] == "O" and board[5] == "O" and board[6] == "O") or \
-        (board[7] == "O" and board[8] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[4] == "O" and board[7] == "O") or \
-        (board[2] == "O" and board[5] == "O" and board[8] == "O") or \
-        (board[3] == "O" and board[6] == "O" and board[9] == "O") or \
-        (board[1] == "O" and board[5] == "O" and board[9] == "O") or \
-        (board[3] == "O" and board[5] == "O" and board[7] == "O"):
+    if is_player_winner(board, "O"):
             os.system("clear")
             print_header()
             print_board()
             print("O wins! Congratulations.")
             break   
         
-    # Check for a tie (is the board full?)
-    is_board_full = True
-    for i in range(1, 10):
-        if board[i] == " ":
-            is_board_full = False
-            break  
-    
     # If board is full, then do this
-    if is_board_full == True:
+    if is_board_full(board):
         print("Tie!")
         break
