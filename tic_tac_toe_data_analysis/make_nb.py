8a21652c19dad178649ea6d633fd6316126a48e1
Patrick Pflughaupt <43568412+patrickpflughaupt@users.noreply.github.com>
2019-04-07 21:44:26 +0100

    Add files via upload

---
+++ tic_tac_toe_data_analysis/make_nb.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+"""
+Create a notebook containing code from a script.
+Run as : python3 make_nb.py my_script.py
+"""
+
+import sys
+
+import nbformat
+from nbformat.v4 import new_notebook, new_code_cell
+
+nb = new_notebook()
+with open (sys.argv[1]) as f:
+    code = f.read()
+
+nb.cells.append(new_code_cell(code))
+nbformat.write(nb, sys.argv[1]+'.ipynb') 
