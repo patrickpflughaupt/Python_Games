025c80de129d0dbec0fd7d8bed441d416af19069
Patrick Pflughaupt <43568412+patrickpflughaupt@users.noreply.github.com>
2019-04-07 21:54:09 +0100

    Delete tictactoe_jup.sh

--- tic_tac_toe_data_analysis/tictactoe_jup.sh
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/bash
-# Convert python file to notebook
-# Run notebook
-
-python3 make_nb.py tictactoe_analysis.py
-mv tictactoe_analysis.py.* tictactoe_analysis.ipynb
-jupyter notebook tictactoe_analysis.ipynb
-
